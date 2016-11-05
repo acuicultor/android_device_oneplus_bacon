00438c40de55f17319efbabb6d3e6b6ff3b5b127
Arne Coucheron <arco68@gmail.com>
2016-11-05 19:13:10 +0200

    bacon: Add shim for libwvm.so
    Change-Id: I1ac58a108783ae1aabe3f3f061b2444925f684db

--- bacon.mk
+++ bacon.mk
@@ -237,6 +237,10 @@ PRODUCT_BOOT_JARS += telephony-ext
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine-8974.conf
 
+# Widevine
+PRODUCT_PACKAGES += \
+    libshim_wvm
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
